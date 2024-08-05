df18694c8677ab1e8d117c20271ae93c5d7419ab
Boo_oyo <136545068+Boooyo@users.noreply.github.com>
2024-08-05 13:40:29 +0900

    Error.java Commit

--- Error.java
+++ Error.java
@@ -109,85 +109,102 @@ public class Error {
 
     static {
         // 오류 메시지 맵 초기화
-        errorMessages.put(EXCEPTION_ERR, "프로그램 예외");
-        errorMessages.put(MD_STATUS_NOT_SETTEMENT_ERR, "먼저 배치 정산을 수행하십시오");
-        errorMessages.put(USER_CANCEL_INPUT, "사용자 취소");
-        errorMessages.put(INPUT_ERR, "입력 오류");
-        errorMessages.put(INPUT_TIME_OUT, "입력 시간 초과");
-        errorMessages.put(UPDATE_WK_ERROR, "작업 키 업데이트 실패");
-        errorMessages.put(UPDATE_BATCH_NO_ERROR, "배치 번호 업데이트 실패");
-        errorMessages.put(SALE_VOID_ADD_TIP, "취소된 거래에 팁을 추가할 수 없습니다");
-        errorMessages.put(OLD_TRADE_HAVE_ADD_TIP, "원래 거래에 이미 팁이 추가되었습니다");
-        errorMessages.put(ADD_TIP_ERROR, "내부 카드에서 팁 추가 실패");
-        errorMessages.put(OLD_TRADE_HAVE_ADJUST, "원래 거래가 조정되었으므로 다시 조정할 수 없습니다");
-        errorMessages.put(CONTACT_ISSUING_BANK_BANK, "발급 은행에 문의하십시오");
-        errorMessages.put(FD_ERR, "도메인 오류");
-        errorMessages.put(FD_NOT_EXIST, "도메인 존재하지 않음");
-        errorMessages.put(UPDATE_RT_ERR, "시계 업데이트 실패");
-        errorMessages.put(NOT_SUPPORT_ADD_TIP, "터미널에서 팁 추가를 지원하지 않습니다");
-        errorMessages.put(NO_PARAM_DOWN, "백엔드에서 파라미터가 다운로드되지 않았습니다");
-        errorMessages.put(SERVICE_RESP_ERR, "백엔드 응답 오류");
-        errorMessages.put(DOWN_PARAM_FAIL, "파라미터 다운로드 실패");
-        errorMessages.put(DOWN_PARA_FD62_LEN_ERR, "응답 메시지 필드 62 길이 오류");
-        errorMessages.put(ORIGNAL_TXN_NOT_RIGHT, "원래 거래 유형이 잘못되었습니다");
-        errorMessages.put(ORIGNAL_TXN_NOT_EXIST, "원래 거래가 존재하지 않습니다");
-        errorMessages.put(NOT_TRADE_REC, "거래 기록이 없습니다");
-        errorMessages.put(OLD_TRADE_HAVE_VOID, "원래 거래가 이미 취소되었습니다");
-        errorMessages.put(OLD_TRADE_CANNOT_VOID, "원래 거래에 팁이 추가되어 취소할 수 없습니다");
-        errorMessages.put(TRADE_ID_NOT_RIGHT, "거래 ID 오류");
-        errorMessages.put(NOT_TRADE_REC_NEED_UPLOAD, "업로드할 거래가 없습니다");
-        errorMessages.put(NO_TXN_CAN_BE_PRINT, "인쇄할 거래 기록이 없습니다");
-        errorMessages.put(NOT_HAVE_CERT_FILE, "인증서 파일이 없습니다");
-        errorMessages.put(OPEN_CERT_FILE_ERR, "인증서 파일 열기 실패");
-        errorMessages.put(POS_HAVE_NOT_LOGIN, "POS가 로그인되지 않았습니다");
-        errorMessages.put(NOT_HAVE_ENOUGH_FLASH, "저장 공간이 부족합니다");
-        errorMessages.put(VOUCHER_FULL, "바우처가 가득 찼습니다. 먼저 정산하십시오");
-        errorMessages.put(TRADE_TOTAL_NUM_FULL, "거래 수가 가득 찼습니다. 먼저 정산하십시오");
-        errorMessages.put(NOT_SUPPORT_CURRENT_TRADE, "현재 거래를 지원하지 않습니다");
-        errorMessages.put(CAN_NOT_REPRINT_REFUSED_REC, "오프라인 IC 카드 거부 거래는 재인쇄할 수 없습니다");
-        errorMessages.put(NOT_ENOUGH_RECHARGE_AMOUNT, "충전 금액이 한도를 초과했습니다");
-        errorMessages.put(NOT_APP_LIST_REC, "IC 카드 파라미터가 없습니다");
-        errorMessages.put(NOT_CAPK_REC, "IC 카드 공개 키가 없습니다");
-        errorMessages.put(GET_SYSTEM_FREE_SPACE_ERR, "시스템 여유 공간 가져오기 실패");
-        errorMessages.put(NOT_FIND_CAPK, "공개 키를 찾을 수 없습니다");
-        errorMessages.put(NOT_FIND_AID, "IC 카드 파라미터를 찾을 수 없습니다");
-        errorMessages.put(INVALID_CARD_NO, "카드 번호가 유효하지 않습니다");
-        errorMessages.put(TDK_ENCRYPT_FAILED, "트랙 데이터 암호화 실패");
-        errorMessages.put(OFFLINE_NUM_FULL, "오프라인 거래 수가 한도를 초과했습니다");
-        errorMessages.put(DELETE_UPFAILED_REC_ERR, "업로드 실패 거래 삭제 실패");
-        errorMessages.put(OFFLINE_AMOUNT_FULL, "오프라인 거래 금액이 한도를 초과했습니다");
-        errorMessages.put(NOT_FAILED_OFFLINE_TRADE, "실패한 오프라인 거래 세부정보가 없습니다");
-        errorMessages.put(NOT_CURRENT_COMM_MODE, "지원되지 않는 통신 모드");
-        errorMessages.put(INIT_COMM_ERR, "통신 초기화 실패");
-        errorMessages.put(CONNECT_HOST_ERR, "호스트 연결 실패");
-        errorMessages.put(SEND_ERR, "데이터 전송 오류");
-        errorMessages.put(RECV_TIMEOUT, "수신 시간 초과");
-        errorMessages.put(RECV_ERR, "데이터 수신 오류");
-        errorMessages.put(UNPACK8583_LEN_ERR, "8583 메시지 길이 오류");
-        errorMessages.put(READ_REVERSAL_ERR, "반전 데이터 읽기 실패");
-        errorMessages.put(SAVE_REVERSAL_ERR, "반전 데이터 저장 실패");
-        errorMessages.put(SAVE_TRADE_ERR, "거래 저장 실패");
-        errorMessages.put(READ_TRADE_ERR, "거래 읽기 실패");
-        errorMessages.put(SAVE_EMV_FILE_ERR, "EMV 파일 저장 실패");
-        errorMessages.put(READ_EMV_FILE_ERR, "EMV 파일 읽기 실패");
-        errorMessages.put(VERIFY_CRC_ERR, "CRC 체크 오류");
-        errorMessages.put(SAVE_PARAM_ERR, "파라미터 저장 실패");
-        errorMessages.put(READ_PARAM_ERR, "파라미터 읽기 실패");
-        errorMessages.put(SAVE_SCRIPTRESULT_ERR, "스크립트 결과 저장 실패");
-        errorMessages.put(READ_SCRIPTRESULT_ERR, "스크립트 결과 읽기 실패");
-        errorMessages.put(UPDATE_TRADE_ERR, "거래 데이터 업데이트 실패");
-        errorMessages.put(PRN_STATUS_PAPEROUT, "프린터 용지 없음");
-        errorMessages.put(PRN_STATUS_TOOHEAT, "프린터 과열");
-        errorMessages.put(PRN_STATUS_FAULT, "프린터 결함");
-        errorMessages.put(PRINT_FAULT_ERR, "인쇄 결함 오류");
-        errorMessages.put(EMV_NOT_INIT, "EMV 초기화되지 않음");
-        errorMessages.put(EMV_TRANS_NOT_START, "EMV 거래 시작되지 않음");
-        errorMessages.put(EMV_INVALID_PARAM, "유효하지 않은 EMV 파라미터");
-        errorMessages.put(EMV_READ_ICC_ERR, "ICC 읽기 실패");
-        errorMessages.put(EMV_WRITE_ICC_ERR, "ICC 쓰기 실패");
-        errorMessages.put(EMV_ICC_NOT_FOUND, "ICC 찾을 수 없음");
-        errorMessages.put(EMV_TIMEOUT, "EMV 작업 시간 초과");
-        errorMessages.put(EMV_PROCESS_ERR, "EMV 처리 오류");
+        initializeErrorMessages();
+    }
+
+    private static void initializeErrorMessages() {
+        // 논리 오류 코드 메시지
+        addErrorMessage(EXCEPTION_ERR, "프로그램 예외");
+        addErrorMessage(MD_STATUS_NOT_SETTEMENT_ERR, "먼저 배치 정산을 수행하십시오");
+        addErrorMessage(USER_CANCEL_INPUT, "사용자 취소");
+        addErrorMessage(INPUT_ERR, "입력 오류");
+        addErrorMessage(INPUT_TIME_OUT, "입력 시간 초과");
+        addErrorMessage(UPDATE_WK_ERROR, "작업 키 업데이트 실패");
+        addErrorMessage(UPDATE_BATCH_NO_ERROR, "배치 번호 업데이트 실패");
+        addErrorMessage(SALE_VOID_ADD_TIP, "취소된 거래에 팁을 추가할 수 없습니다");
+        addErrorMessage(OLD_TRADE_HAVE_ADD_TIP, "원래 거래에 이미 팁이 추가되었습니다");
+        addErrorMessage(ADD_TIP_ERROR, "내부 카드에서 팁 추가 실패");
+        addErrorMessage(OLD_TRADE_HAVE_ADJUST, "원래 거래가 조정되었으므로 다시 조정할 수 없습니다");
+        addErrorMessage(CONTACT_ISSUING_BANK_BANK, "발급 은행에 문의하십시오");
+        addErrorMessage(FD_ERR, "도메인 오류");
+        addErrorMessage(FD_NOT_EXIST, "도메인 존재하지 않음");
+        addErrorMessage(UPDATE_RT_ERR, "시계 업데이트 실패");
+        addErrorMessage(NOT_SUPPORT_ADD_TIP, "터미널에서 팁 추가를 지원하지 않습니다");
+        addErrorMessage(NO_PARAM_DOWN, "백엔드에서 파라미터가 다운로드되지 않았습니다");
+        addErrorMessage(SERVICE_RESP_ERR, "백엔드 응답 오류");
+        addErrorMessage(DOWN_PARAM_FAIL, "파라미터 다운로드 실패");
+        addErrorMessage(DOWN_PARA_FD62_LEN_ERR, "응답 메시지 필드 62 길이 오류");
+        addErrorMessage(ORIGNAL_TXN_NOT_RIGHT, "원래 거래 유형이 잘못되었습니다");
+        addErrorMessage(ORIGNAL_TXN_NOT_EXIST, "원래 거래가 존재하지 않습니다");
+        addErrorMessage(NOT_TRADE_REC, "거래 기록이 없습니다");
+        addErrorMessage(OLD_TRADE_HAVE_VOID, "원래 거래가 이미 취소되었습니다");
+        addErrorMessage(OLD_TRADE_CANNOT_VOID, "원래 거래에 팁이 추가되어 취소할 수 없습니다");
+        addErrorMessage(TRADE_ID_NOT_RIGHT, "거래 ID 오류");
+        addErrorMessage(NOT_TRADE_REC_NEED_UPLOAD, "업로드할 거래가 없습니다");
+        addErrorMessage(NO_TXN_CAN_BE_PRINT, "인쇄할 거래 기록이 없습니다");
+        addErrorMessage(NOT_HAVE_CERT_FILE, "인증서 파일이 없습니다");
+        addErrorMessage(OPEN_CERT_FILE_ERR, "인증서 파일 열기 실패");
+        addErrorMessage(POS_HAVE_NOT_LOGIN, "POS가 로그인되지 않았습니다");
+        addErrorMessage(NOT_HAVE_ENOUGH_FLASH, "저장 공간이 부족합니다");
+        addErrorMessage(VOUCHER_FULL, "바우처가 가득 찼습니다. 먼저 정산하십시오");
+        addErrorMessage(TRADE_TOTAL_NUM_FULL, "거래 수가 가득 찼습니다. 먼저 정산하십시오");
+        addErrorMessage(NOT_SUPPORT_CURRENT_TRADE, "현재 거래를 지원하지 않습니다");
+        addErrorMessage(CAN_NOT_REPRINT_REFUSED_REC, "오프라인 IC 카드 거부 거래는 재인쇄할 수 없습니다");
+        addErrorMessage(NOT_ENOUGH_RECHARGE_AMOUNT, "충전 금액이 한도를 초과했습니다");
+        addErrorMessage(NOT_APP_LIST_REC, "IC 카드 파라미터가 없습니다");
+        addErrorMessage(NOT_CAPK_REC, "IC 카드 공개 키가 없습니다");
+        addErrorMessage(GET_SYSTEM_FREE_SPACE_ERR, "시스템 여유 공간 가져오기 실패");
+        addErrorMessage(NOT_FIND_CAPK, "공개 키를 찾을 수 없습니다");
+        addErrorMessage(NOT_FIND_AID, "IC 카드 파라미터를 찾을 수 없습니다");
+        addErrorMessage(INVALID_CARD_NO, "카드 번호가 유효하지 않습니다");
+        addErrorMessage(TDK_ENCRYPT_FAILED, "트랙 데이터 암호화 실패");
+        addErrorMessage(OFFLINE_NUM_FULL, "오프라인 거래 수가 한도를 초과했습니다");
+        addErrorMessage(DELETE_UPFAILED_REC_ERR, "업로드 실패 거래 삭제 실패");
+        addErrorMessage(OFFLINE_AMOUNT_FULL, "오프라인 거래 금액이 한도를 초과했습니다");
+        addErrorMessage(NOT_FAILED_OFFLINE_TRADE, "실패한 오프라인 거래 세부정보가 없습니다");
+
+        // 통신 오류 코드 메시지
+        addErrorMessage(NOT_CURRENT_COMM_MODE, "지원되지 않는 통신 모드");
+        addErrorMessage(INIT_COMM_ERR, "통신 초기화 실패");
+        addErrorMessage(CONNECT_HOST_ERR, "호스트 연결 실패");
+        addErrorMessage(SEND_ERR, "데이터 전송 오류");
+        addErrorMessage(RECV_TIMEOUT, "수신 시간 초과");
+        addErrorMessage(RECV_ERR, "데이터 수신 오류");
+        addErrorMessage(UNPACK8583_LEN_ERR, "8583 메시지 길이 오류");
+
+        // 데이터 읽기/쓰기 오류 코드 메시지
+        addErrorMessage(READ_REVERSAL_ERR, "반전 데이터 읽기 실패");
+        addErrorMessage(SAVE_REVERSAL_ERR, "반전 데이터 저장 실패");
+        addErrorMessage(SAVE_TRADE_ERR, "거래 저장 실패");
+        addErrorMessage(READ_TRADE_ERR, "거래 읽기 실패");
+        addErrorMessage(SAVE_EMV_FILE_ERR, "EMV 파일 저장 실패");
+        addErrorMessage(READ_EMV_FILE_ERR, "EMV 파일 읽기 실패");
+        addErrorMessage(VERIFY_CRC_ERR, "CRC 체크 오류");
+        addErrorMessage(SAVE_PARAM_ERR, "파라미터 저장 실패");
+        addErrorMessage(READ_PARAM_ERR, "파라미터 읽기 실패");
+        addErrorMessage(SAVE_SCRIPTRESULT_ERR, "스크립트 결과 저장 실패");
+        addErrorMessage(READ_SCRIPTRESULT_ERR, "스크립트 결과 읽기 실패");
+        addErrorMessage(UPDATE_TRADE_ERR, "거래 데이터 업데이트 실패");
+
+        // 프린터 오류 코드 메시지
+        addErrorMessage(PRN_STATUS_PAPEROUT, "프린터 용지 없음");
+        addErrorMessage(PRN_STATUS_TOOHEAT, "프린터 과열");
+        addErrorMessage(PRN_STATUS_FAULT, "프린터 결함");
+        addErrorMessage(PRINT_FAULT_ERR, "인쇄 결함 오류");
+
+        // EMV 관련 오류 코드 메시지
+        addErrorMessage(EMV_NOT_INIT, "EMV 초기화되지 않음");
+        addErrorMessage(EMV_TRANS_NOT_START, "EMV 거래 시작되지 않음");
+        addErrorMessage(EMV_INVALID_PARAM, "유효하지 않은 EMV 파라미터");
+        addErrorMessage(EMV_READ_ICC_ERR, "ICC 읽기 실패");
+        addErrorMessage(EMV_WRITE_ICC_ERR, "ICC 쓰기 실패");
+        addErrorMessage(EMV_ICC_NOT_FOUND, "ICC 찾을 수 없음");
+        addErrorMessage(EMV_TIMEOUT, "EMV 작업 시간 초과");
+        addErrorMessage(EMV_PROCESS_ERR, "EMV 처리 오류");
+    }
+
+    private static void addErrorMessage(int code, String message) {
+        errorMessages.put(code, message);
     }
 
     /**
